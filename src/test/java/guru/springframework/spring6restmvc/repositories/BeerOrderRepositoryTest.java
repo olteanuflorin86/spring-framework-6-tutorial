b70f61f556edfd78dfbb21faf6943fa33fabf185
Florin Olteanu <olteanuflorin86@gmail.com>
2024-02-06 10:17:27 +0200

    adding persistence test (+ correct MySQL script)

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerOrder;
 import guru.springframework.spring6restmvc.entities.Customer;
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 public class BeerOrderRepositoryTest {
@@ -28,14 +30,24 @@ public class BeerOrderRepositoryTest {
         testCustomer = customerRepository.findAll().get(0);
         testBeer = beerRepository.findAll().get(0);
     }
-
+    
+    @Transactional
     @Test
     void testBeerOrders() {
-        System.out.println(beerOrderRepository.count());
-        System.out.println(customerRepository.count());
-        System.out.println(beerRepository.count());
-        System.out.println(testCustomer.getName());
-        System.out.println(testBeer.getBeerName());
+//        System.out.println(beerOrderRepository.count());
+//        System.out.println(customerRepository.count());
+//        System.out.println(beerRepository.count());
+//        System.out.println(testCustomer.getName());
+//        System.out.println(testBeer.getBeerName());
+    	
+    	BeerOrder beerOrder = BeerOrder.builder()
+    			.customerRef("Test order")
+    			.customer(testCustomer)
+    			.build();
+    	
+    	BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+    	
+    	System.out.println(savedBeerOrder.getCustomerRef());
     }
 
 }
