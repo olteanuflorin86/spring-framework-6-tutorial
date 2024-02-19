720c02d24f5b21fa89553cfd149699d87619b568
Florin Olteanu <olteanuflorin86@gmail.com>
2024-02-19 09:52:23 +0200

    customize URL/ entity path

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -5,12 +5,14 @@ import guru.springframework.spring6restmvc.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
