ed97b8d313caa9859a5b0f888c3b466627ac215a
Florin Olteanu <olteanuflorin86@gmail.com>
2024-02-20 09:18:32 +0200

    Initial setup

--- src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
@@ -1,7 +1,5 @@
 package guru.springframework.spring6restmvc.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,24 +13,23 @@ import java.util.UUID;
 @Builder
 @Data
 public class BeerDTO {
+	
     private UUID id;
+    
     private Integer version;
 
-    @NotBlank
-    @NotNull
     private String beerName;
 
-    @NotNull
     private BeerStyle beerStyle;
 
-    @NotNull
-    @NotBlank
     private String upc;
+
     private Integer quantityOnHand;
 
-    @NotNull
     private BigDecimal price;
+    
     private LocalDateTime createdDate;
+    
     private LocalDateTime updateDate;
 
 }
