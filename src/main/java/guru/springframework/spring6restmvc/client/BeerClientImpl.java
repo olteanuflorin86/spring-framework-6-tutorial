5d52fd9a365f3f3ec671732e95aca95afac16abd
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-01 18:42:00 +0200

    configure RestTemplateBuilder (with default BASE_URL)

--- src/main/java/guru/springframework/spring6restmvc/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/client/BeerClientImpl.java
@@ -20,7 +20,7 @@ public class BeerClientImpl implements BeerClient {
 	
 	private final RestTemplateBuilder restTemplateBuilder;
 	
-    private static final String BASE_URL = "http://localhost:8080";
+//    private static final String BASE_URL = "http://localhost:8080";
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
 	@Override
@@ -30,15 +30,18 @@ public class BeerClientImpl implements BeerClient {
 		
 //		ResponseEntity<String> stringResponse = restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
 		ResponseEntity<String> stringResponse = 
-				restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+//				restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+				restTemplate.getForEntity(GET_BEER_PATH, String.class);
 		
 		
 		ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+//              restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+				restTemplate.getForEntity(GET_BEER_PATH, Map.class);
 		
 		
 		ResponseEntity<JsonNode> jsonResponse =
-				restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
+//				restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
+				restTemplate.getForEntity(GET_BEER_PATH, JsonNode.class);
 		
 		jsonResponse.getBody().findPath("content")
 				.elements().forEachRemaining(node -> {
@@ -46,7 +49,8 @@ public class BeerClientImpl implements BeerClient {
 				});
 		
 		ResponseEntity<BeerDTOPageImpl> beerDTOResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , BeerDTOPageImpl.class);
+//              restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , BeerDTOPageImpl.class);
+				restTemplate.getForEntity(GET_BEER_PATH , BeerDTOPageImpl.class);
 		
 		
 		System.out.println(beerDTOResponse.getBody());
