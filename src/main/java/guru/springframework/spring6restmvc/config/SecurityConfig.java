6c1cb6438e01bb52848a87a251ba8e31781d90a9
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-10 18:20:20 +0200

    add User Details Service

--- src/main/java/guru/springframework/spring6restmvc/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SecurityConfig.java
@@ -7,9 +7,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -33,7 +37,7 @@ public class SecurityConfig {
 
         return http.build();
     }
-    
+
     @Bean
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
@@ -48,5 +52,16 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
 
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+    
 }
