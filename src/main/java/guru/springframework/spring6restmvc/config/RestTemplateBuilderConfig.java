0060f14e0b9a3b80491d4b628e5a341ef1cc151a
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-10 09:38:27 +0200

    setup HTTP Basic with RestTemplate

--- src/main/java/guru/springframework/spring6restmvc/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/RestTemplateBuilderConfig.java
@@ -12,6 +12,12 @@ public class RestTemplateBuilderConfig {
 	
 	@Value("${rest.template.rootUrl}")
 	String rootUrl;
+	
+    @Value("${rest.template.username}")
+    String username;
+
+    @Value("${rest.template.password}")
+    String password;
 
 	@Bean
 	RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
@@ -23,6 +29,9 @@ public class RestTemplateBuilderConfig {
 //		DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory("http://localhost:8080");
 		DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
 		
-		return builder.uriTemplateHandler(uriBuilderFactory);
+        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username,password);
+
+//        return builder.uriTemplateHandler(uriBuilderFactory);
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
 	}
 }
