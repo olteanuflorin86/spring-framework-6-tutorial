3fe5adebddd738e14a3c5b533ea09efdf54e422c
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-14 07:06:00 +0200

    add WebFlux Controller example (listBeers mock)

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerDTO.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvc.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+    
+}
