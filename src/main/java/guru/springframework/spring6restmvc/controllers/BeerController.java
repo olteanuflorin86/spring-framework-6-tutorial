0a0e35ce17332a56956e6c02102f3bd09af4b429
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-15 09:54:59 +0200

    add createNewBeer method

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -1,8 +1,14 @@
 package guru.springframework.spring6restmvc.controllers;
 
+import java.util.concurrent.atomic.AtomicInteger; 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.services.BeerService;
@@ -29,5 +35,20 @@ public class BeerController {
 	@GetMapping(BEER_PATH_ID)
 	Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
 		return beerService.getBeerById(beerId);
+	}
+	
+	@PostMapping(BEER_PATH)
+	ResponseEntity<Void> createNewBeer(@RequestBody BeerDTO beerDTO) {
+		
+		AtomicInteger atomicInteger = new AtomicInteger();
+		
+		beerService.saveNewBeer(beerDTO).subscribe(savedBeerDTO -> {
+			atomicInteger.set(savedBeerDTO.getId());
+		});
+		
+		return ResponseEntity.created(UriComponentsBuilder
+					.fromHttpUrl("http://localhost:8080/" + BEER_PATH + "/" + atomicInteger.get())
+					.build().toUri())
+				.build();
 	}
 }
--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -28,4 +28,10 @@ public class BeerServiceImpl implements BeerService {
 				.map(beerMapper::beerToBeerDto);
 	}
 
+	@Override
+    public Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO) {
+        return beerRepository.save(beerMapper.beerDtoToBeer(beerDTO))
+                	.map(beerMapper::beerToBeerDto);
+    }
+
 }
