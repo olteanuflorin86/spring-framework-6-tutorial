e39c6b9a6bc94646ba4454ba3d617432a881a02f
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-22 10:41:32 +0200

    adding Webclient Global Configuration

---
+++ src/main/java/guru/springframework/spring6restmvc/config/WebClientConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+
+@Configuration
+public class WebClientConfig implements WebClientCustomizer {
+
+	private final String rootUrl;
+	
+	public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl) {
+		this.rootUrl = rootUrl;
+	}
+	
+	@Override
+	public void customize(Builder webClientBuilder) {
+		webClientBuilder.baseUrl(rootUrl);
+	}
+
+}
