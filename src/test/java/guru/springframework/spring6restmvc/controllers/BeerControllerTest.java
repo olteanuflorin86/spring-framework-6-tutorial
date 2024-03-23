d2ffd38f16a22f006f195a19559feaed8ded7e84
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-23 06:31:58 +0200

    add Maven dependencies and do Configuration to pass our failling controller tests (because we added security)

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring6restmvc.controllers;
 
-import org.junit.jupiter.api.MethodOrderer; 
+import org.junit.jupiter.api.MethodOrderer;  
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
