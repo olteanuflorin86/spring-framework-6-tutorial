22ee0dd813867970035a25d329f917a904970332
Florin Olteanu <olteanuflorin86@gmail.com>
2024-03-25 14:31:54 +0200

    add Spring Security configuration we need for OAuth2 Resource Server

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,7 +2,7 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;  
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,12 +10,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecConfig {
 
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests()
 			.anyRequest().authenticated()
-			.and().httpBasic(Customizer.withDefaults())
-			.csrf().ignoringRequestMatchers("/api/**");
+			.and()
+//			.httpBasic(Customizer.withDefaults())
+//			.csrf().ignoringRequestMatchers("/api/**");
+			.oauth2ResourceServer().jwt();
 		
 		return http.build();
 	}
